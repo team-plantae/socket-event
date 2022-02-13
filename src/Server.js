c0fe923a2a4fb589a91fcd40dadf326d902f8a59
giovani.freitag <giovani.freitag@plantae.agr.br>
2022-02-13 16:29:23 -0400

    Adicionado evento de desconexão do cliente

--- src/Server.js
+++ src/Server.js
@@ -26,16 +26,29 @@ class Server{
 
     listen(){
 
-        this.server.on('connection', socket => this.handleSocketConnect(socket));
+        this.server.on('connection', socket => this.handleSocketConnection(socket));
         this.server.listen(this.port);
     }
 
-    handleSocketConnect(socket){
+    handleSocketConnection(socket){
 
         let eventSocket = new Socket(socket);
         this.sockets.push(eventSocket);
 
         this.eventHandler.emit('connection', eventSocket);
+
+        socket.on('end', () => this.handleSocketDisconnect(eventSocket));
+    }
+
+    handleSocketDisconnection(eventSocket){
+
+        var pos = this.sockets.indexOf(eventSocket);
+
+        if(!~pos){
+            this.sockets.splice(pos, 1);
+        }
+
+        this.eventHandler.emit('disconnection', eventSocket);
     }
 
     on(...args){
